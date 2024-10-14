9c25328d177a659251ca108a7ceb079f21cfbfa6
ibharatsaini <sainib601@gmail.com>
2024-10-15 03:39:19 +0530

    redirecting to reviews

--- src/pages/api/redirector.ts
+++ src/pages/api/redirector.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 const redirector=  (req:NextRequest , res:NextResponse) => {
-    return NextResponse.redirect(new URL('http://localhost:3000'))
+    return NextResponse.redirect(new URL('http://codeiq.vercel.app/reviews'))
 }
 
 export const runtime = 'edge'
