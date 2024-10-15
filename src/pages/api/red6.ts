4d8fcff90ca70b80b27792c89d148e0b61586b17
ibharatsaini <sainib601@gmail.com>
2024-10-15 21:32:45 +0530

    rbndr fixed

--- src/pages/api/red6.ts
+++ src/pages/api/red6.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 const redirector2 = (req: NextRequest, res: NextResponse) => {
-  return NextResponse.redirect(new URL("https://a9fea9fe.c0a80001.rbndr.us/latest/meta-data/hostname"));
+  return NextResponse.redirect(new URL("https://7f000001.a9fea9fe.rbndr.us:8080/latest/meta-data/hostname"));
 };
 
 export const runtime = "edge";
