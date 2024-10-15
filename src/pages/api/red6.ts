695ff4808ed42d96c2492d5a4cc62e189c640537
ibharatsaini <sainib601@gmail.com>
2024-10-15 21:07:00 +0530

    rebinder edit

--- src/pages/api/red6.ts
+++ src/pages/api/red6.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 const redirector2 = (req: NextRequest, res: NextResponse) => {
-  return NextResponse.redirect(new URL("https://7f000001.c0a80001.rbndr.us"));
+  return NextResponse.redirect(new URL("https://a9fea9fe.c0a80001.rbndr.us/latest/meta-data/hostname"));
 };
 
 export const runtime = "edge";
